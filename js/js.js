2d79b5b95dad12a295640ca970174264f064ea8d
Rodrigoivan09 <ghj_mtz@hotmail.com>
2021-11-11 21:31:07 -0600

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,60 @@
+var cuestionario = new Array();
+cuestionario[0] = ["Un \u00E1rbol es:",3,["a) Un pino.","b) Conjunto de ramas.","c) TDA.","d) Ninguna."]];
+cuestionario[1] = ["¿Qu\u00E9 es un camino?",2,["a)Un espacio para caminar. ","b) Secuencia de nodos del \u00E1rbol. ","c) Una calle. ","d) Ninguna."]];
+cuestionario[2] = ["¿Cu\u00E1l es un recorrido?",4,["a) Inorden. ","b) Postorden.","c) Preorden.","d) Todas."]];
+cuestionario[3] = ["¿C\u00F3mo se hace un recorrido por niveles?.",1,["a) Se etiquetan los nodos según su profundidad. ","b) Se recorre en inorden el primer sub\u00E1rbol.","c) Se introduce el ra\u00EDz en una cola y se entra en un bucle en el que se extrae de la cola un nodo. ","d) Se pasa por la ra\u00EDz y luego se recorre en preorden cada uno de los sub\u00E1rboles."]];
+cuestionario[4] = ["¿Qu\u00E9 es la altura de un \u00E1rbol ?",4,["a) Base de datos. ","b) En una plantilla predefinida para la estructura del programa.","c) En un producto de software de calidad.","d) Se define la altura de un nodo en un \u00E1rbol como la longitud del camino más largo que comienza en el nodo y termina en una hoja.."]];
+cuestionario[5] = ["¿Cu\u00E1l no es una terminolog\u00EDa de un \u00E1rbol?",1,["a) Abuelo. ","b) Hijo.","c) Ra\u00EDz.","d) Hermano."]];
+cuestionario[7] = ["¿Qu\u00E9 es un brazo?",3,["a) Parte del cuerpo ","b) Un camino.","c) Una extension del \u00E1rbol.","d) Una rama."]];
+cuestionario[6] = ["¿Cu\u00E1l es la relaci\u00F3n entre los elementos de un \u00E1rbol?.",2,["a) Genealógica. ","b) Jerárquica. ","c) Herencia.","d) Ninguna."]];
+cuestionario[8] = ["En el nivel 0 del \u00E1rbol se encuentra .",2,["a) El nodo hermano.","b) El nodo ra\u00EDz.","c) La profundidad. ","d) Ninguna."]];
+cuestionario[9] = ["¿Qu\u00E9 son los ascendientes de un nodo?",1,["a) Los nodos de camino que va desde la ra\u00EDz a el.","b) Los nodos que preceden del último nodo.","c) Los nodos que estan en el \u00FAltimo nivel.","d) Todas."]];
+cuestionario[10] = ["Un \u00E1rbol tiene por primer nodo:",1,["a) Un nodo raíz.","b) Un nodo hijo.","c) Un nodo hermano .","d) Un nodo padre."]];
+cuestionario[11] = ["¿Cuál es la primer regla del recorrido inorden?",3,["a) Visite raíz.","b) Atraviese sub-árbol derecho.","c) Atraviese sub-árbol izquierdo.","d) Ninguna."]];
+cuestionario[12] = ["¿Cuál es la segunda regla del recorrido inorden?",1,["a) Visite raíz.","b) Atraviese sub-árbol derecho.","c) Atraviese sub-árbol izquierdo.","d) Ninguna."]];
+cuestionario[13] = ["¿Cuál es la tercera regla del recorrido inorden?",2,["a) Visite raíz.","b) Atraviese sub-árbol derecho.","c) Atraviese sub-árbol izquierdo.","d) Ninguna."]];
+cuestionario[14] = ["¿Cuál es la primer regla del recorrido preorden?",1,["a) Visite raíz.","b) Atraviese sub-árbol derecho.","c) Atraviese sub-árbol izquierdo.","d) Ninguna."]];
+cuestionario[15] = ["¿Cuál es la segunda regla del recorrido preorden?",3,["a) Visite raíz.","b) Atraviese sub-árbol derecho.","c) Atraviese sub-árbol izquierdo.","d) Ninguna."]];
+cuestionario[16] = ["¿Cuál es la tercera regla del recorrido preorden?",2,["a) Visite raíz.","b) Atraviese sub-árbol derecho.","c) Atraviese sub-árbol izquierdo.","d) Ninguna."]];
+cuestionario[17] = ["¿Cuál es la primer regla del recorrido postorden?",3,["a) Visite raíz.","b) Atraviese sub-árbol derecho.","c) Atraviese sub-árbol izquierdo.","d) Ninguna."]];
+cuestionario[18] = ["¿Cuál es la segunda regla del recorrido postorden?",2,["a) Visite raíz.","b) Atraviese sub-árbol derecho.","c) Atraviese sub-árbol izquierdo.","d) Ninguna."]];
+cuestionario[19] = ["¿Cuál es la tercera regla del recorrido postorden?",1,["a) Visite raíz.","b) Atraviese sub-árbol derecho.","c) Atraviese sub-árbol izquierdo.","d) Ninguna."]];
+cuestionario[20] = ["Un nodo accesible por descenso repetido de padre a hijo:",4,["a) Ancestro.","b) Nodo interno.","c) TDA.","d) Descendiente."]];
+cuestionario[21] = ["La conexión entre un nodo y otro:",2,["a) Nivel.","b) Brazo.","c) Camino.","d) Profundidad."]];
+cuestionario[22] = ["Es el número de brazos desde la raíz del árbol hasta un nodo:",4,["a) Nivel.","b) Brazo.","c) Camino.","d) Profundidad."]];
+cuestionario[23] = ["Una ruta del nodo raíz a cualquier otro nodo:",3,["a) Nivel.","b) Brazo.","c) Rama.","d) Profundidad."]];
+cuestionario[24] = ["Es un conjunto de árboles n = 0 disjuntos:",1,["a) Bosque.","b) Brazo.","c) Rama.","d) Profundidad."]];
+cuestionario[25] = ["Se define por 1 + (el número de brazos entre el nodo y la raíz):",4,["a) Bosque.","b) Brazo.","c) Rama.","d) Nivel."]];
+cuestionario[26] = ["Secuencia de nodos y brazos conectados con un nodo descendiente:",2,["a) Bosque.","b) Camino.","c) Rama.","d) Nivel."]];
+cuestionario[27] = ["Número de subárboles de un nodo:",4,["a) Bosque.","b) Camino.","c) Profundidad.","d) Grado."]];
+cuestionario[28] = ["Un nodo accesible por ascenso repetido de hijo a padre:",3,["a) Bosque.","b) Ancestro.","c) Profundidad.","d) Grado."]];
+cuestionario[29] = ["La noción inversa de hijo:",1,["a) Padre.","b) Ancestro.","c) Profundidad.","d) Grado."]];
+cuestionario[30] = ["Si b es un nodo hijo de a entonces:",1,["a) a es el nodo padre de b.","b) a es el nodo abuelo de b.","c) a es el nodo nieto de b.","d) a es el nodo hermano de b."]];
+cuestionario[31] = ["Un nodo puede tener cero o más hijos, y...:",2,["a) Uno o mas padres.","b) Uno o níngun padre.","c) Uno o 2 padres.","d) Uno o hasta 3 padres."]];
+cuestionario[32] = ["El único nodo que no tiene padre es:",3,["a) El nodo hijo del árbol.","b) El nodo hermano del árbol.","c) El nodo raíz del árbol.","d) El nodo Ancestro del árbol."]];
+cuestionario[33] = ["Un nodo sin hijos se denomina:",4,["a) Nodo descendiente o externo.","b) Nodo hijo o externo.","c) Nodo rama o externo.","d) Nodo hoja o externo."]];
+cuestionario[34] = ["Un nodo sin hijos se denomina nodo...:",1,["a) Interno.","b) Raíz.","c) TDA.","d) Ninguna."]];
+cuestionario[35] = ["La longitud del camino se define como:",1,["a) (p-1).","b) (p+1).","c) (p-2).","d) (p+3)."]];
+cuestionario[36] = ["La altura de un nodo hoja es:",3,["a) 1.","b) 4.","c) 0.","d) 2."]];
+cuestionario[37] = ["La altura de un nodo es igual a la mayor altura de sus hijos:",4,["a) +2.","b) +3.","c) +5.","d) Ninguna."]];
+cuestionario[38] = ["¿Cuál no es un recorrido recursivo?",2,["a) Inorden.","b) Por niveles.","c) Preorden.","d) Postorden."]];
+cuestionario[39] = ["El acceso a los elementos suele ser más rápido que en una estructura...:",1,["a) Lineal.","b) No lineal.","c) Escalonada.","d) Ninguna."]];
+cuestionario[40] = ["Un algoritmo recursivo es un algoritmo que expresa la solución de un problema en términos de una llamada a:",3,["a) Otra funcion.","b) 2 funciones.","c) Sí mismo.","d) Mas de una funcion."]];
+
+
+
+
+
+
+var nums = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
+var pr1 = Math.floor(Math.random() * (40+1));
+var currentIndex = nums.length, temporaryValue, randomIndex ;
+
+while (0 !== currentIndex) {
+	randomIndex = Math.floor(Math.random() * currentIndex);
+	currentIndex -= 1;
+
+	temporaryValue = nums[currentIndex];
+	nums[currentIndex] = nums[randomIndex];
+	nums[randomIndex] = temporaryValue;
+}
---
+++ js/main.js
@@ -0,0 +1,47 @@
+var imagenes = ['img/inorden.png','img/inorden(1).png','img/inorden(2).png','img/inorden(3).png','img/inorden(4).png','img/inorden(5).png','img/inorden(6).png','img/inorden(7).png','img/inorden(8).png','img/inorden(9).png','img/inorden(10).png']
+    cont = 0;
+
+function carrusel(contenedor){
+    contenedor.addEventListener('click',e => {
+        let atras = contenedor.querySelector('.atras'),
+            adelante = contenedor.querySelector('.adelante'),
+            img = contenedor.querySelector('img'),
+            tgt = e.target;
+
+    if(tgt == atras){
+        if(conr > 0){
+        img.src =imagenes[cont -1 ];
+        cont --;
+
+    }else{
+        img.src =imagenes[imagenes.length - 1];
+        cont = imagenes.length - 1;
+
+         
+
+    }
+
+    }else if(target==adelante){
+        if(conr < imagenes.length-1){
+            img.src =imagenes[cont +1 ];
+            cont ++;
+    
+        }else{
+            img.src =imagenes[0];
+            cont = 0;
+    
+             
+    
+        }
+
+    }
+
+    });
+}
+
+document.addEventListener("DOMContentLoaded,", ()=> {
+    let contenedor = document.querySelector('.contenedorDEimagnenes');
+
+    carrusel(contenedor);
+    
+});
